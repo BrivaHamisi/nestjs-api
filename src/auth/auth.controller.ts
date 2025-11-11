cb074350d0b3e4f3c7e38e74c3a568b425386797
BrivaHamisi <brivahamisi@gmail.com>
2025-11-11 12:59:32 +0300

    Auth Guards and Installed Pactum

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -6,6 +6,7 @@ import { AuthDto } from "./dto";
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    // @HttpCode(HttpStatus.OK)
     @Post('signup')
     signup(@Body() dto: AuthDto){
         console.log('Received DTO:', dto);
@@ -13,6 +14,7 @@ export class AuthController {
         return this.authService.signup(dto);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
     signin(@Body() dto: AuthDto){
         console.log({dto});
